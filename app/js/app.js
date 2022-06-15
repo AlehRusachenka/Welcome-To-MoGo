f7555b4ed0ffc36e28bc166a0d0a6f24f5a69bef
Aleh.R <aleh.rusachenka@gmail.com>
2022-06-15 20:39:53 +0300

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+// // Import vendor jQuery plugin example
+// import '~/app/libs/mmenu/dist/mmenu.js'
+
+document.addEventListener("DOMContentLoaded", () => {
+  let header = document.querySelector(".header");
+
+  document.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 1) {
+      header.classList.add("header_has-bg");
+    } else {
+      header.classList.remove("header_has-bg");
+    }
+  });
+});
