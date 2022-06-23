366166716ed16574f543ab9c6f937084f41a8100
Aleh.R <aleh.rusachenka@gmail.com>
2022-06-23 16:46:51 +0300

    new sections added, tablet and mobile adaprive added

--- app/js/app.js
+++ app/js/app.js
@@ -10,6 +10,16 @@ document.addEventListener("DOMContentLoaded", () => {
     pagination: false,
     type: "loop",
     perPage: 1,
+    gap: "40px",
+    breakpoints: {
+      560: {
+        arrows: false,
+        pagination: true,
+        classes: {
+          pagination: "splide__pagination review-slider__pagination",
+        },
+      },
+    },
   }).mount();
 });
 
@@ -21,9 +31,54 @@ document.addEventListener("DOMContentLoaded", () => {
     pagination: false,
     type: "loop",
     perPage: 1,
+    gap: "40px",
+    breakpoints: {
+      560: {
+        arrows: false,
+        pagination: true,
+        classes: {
+          pagination: "splide__pagination review-slider__pagination",
+        },
+      },
+    },
   }).mount();
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const promoSlides = document.querySelectorAll("[data-slide-title]");
+  let slideTitles = [];
+
+  promoSlides.forEach(function (slide) {
+    slideTitles.push(slide.dataset.slideTitle);
+  });
+
+  console.log(slideTitles);
+
+  const promoSlider = new Splide(".promo__slider", {
+    arrows: false,
+    drag: false,
+    type: "loop",
+    autoplay: true,
+    interval: 3000,
+    classes: {
+      pagination: "splide__pagination promo__pagination",
+    },
+  });
+
+  promoSlider.on("pagination:mounted", function (data) {
+    // You can add your class to the UL element
+    data.list.classList.add("splide__pagination--custom");
+
+    // `items` contains all dot items
+    data.items.forEach(function (item) {
+      item.button.textContent = `${slideTitles[item.page]}`;
+    });
+  });
+
+  promoSlider.mount();
+});
+
+// Fixed Header
 document.addEventListener("DOMContentLoaded", () => {
   let header = document.querySelector(".header");
 
@@ -33,5 +88,16 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       header.classList.remove("header_has-bg");
     }
+  });
+});
+
+// Burger Menu
+document.addEventListener("DOMContentLoaded", () => {
+  const headerBureger = document.querySelector(".header__burger");
+  const headerNav = document.querySelector(".header__nav");
+
+  headerBureger.addEventListener("click", function (event) {
+    headerBureger.classList.toggle("burger_closed");
+    headerNav.classList.toggle("header__nav_opened");
   });
 });
